b95b8ec6f9a55eeaf519557d4bb9989cfe17b9e7
Zaco <cyrilmathe08@gmail.com>
2025-03-11 09:36:57 +0100

    :art: suppression de 2 fonctions inutiles et mise a 0 du download count quand je créer une file

--- Models/File.Model.php
+++ Models/File.Model.php
@@ -35,33 +35,8 @@ class File {
         return $file;
     }
 
-    public static function getByTitle($title) {
-        $result = Model::find(self::$table, ['title' => $title], 1);
-
-        $file = new self();
-        $file->fileid = $result[0]["file_id"];
-        $file->userid = $result[0]["user_id"];
-        $file->title = $result[0]["title"];
-        $file->filedata = $result[0]["file_data"];
-        $file->downloadcount = $result[0]["download_count"];
-
-        return $file;
-    }
-
-    public static function getByDownloadCount($downloadcount) {
-        $result = Model::find(self::$table, ['download_count' => $downloadcount], 1);
-
-        $file = new self();
-        $file->fileid = $result[0]["file_id"];
-        $file->userid = $result[0]["user_id"];
-        $file->title = $result[0]["title"];
-        $file->filedata = $result[0]["file_data"];
-        $file->downloadcount = $result[0]["download_count"];
-
-        return $file;
-    }
-
-    public static function createFile($userid, $title, $filedata, $downloadcount){
+    public static function createFile($userid, $title, $filedata){
+        $downloadcount = 0;
         $result = Model::insert(self::$table, ["user_id" => $userid, "title" => $title, "file_data" => $filedata, "download_count" => $downloadcount]);
         return $result;
     }
