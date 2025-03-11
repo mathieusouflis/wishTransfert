8bd5a7705a7abe048ca759630af32eb94038e376
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-11 10:09:08 +0100

    :construction: WIP UserModel

--- Models/User.Model.php
+++ Models/User.Model.php
@@ -37,25 +37,75 @@ class User {
     }
 
     public static function create($username, $password, $email){
-        $result = Model::insert(self::$table, ["username"=> $username,"password"=> $password,"email"=> $email]);
-        return $result;
+        if(!self::isUsernameValid($username)){
+            $errors[] = "Username is not valid (minimum 4 characters), only . and _ allowed";
+        }else if (!self::isUsernameUnique($username)){
+            $errors[] = "Username is note unique";
+        }
+        if(!self::isEmailUnique($email)){
+            $errors[] = "Email is allready used";
+        }
+        if(!self::isPasswordValid($password)){
+            $errors[] = "Password must contain at least  8 characters, 1 lowercase, 1 uppercase, 1 number and 1 special characters (@$!%*?&)";
+        }
+
+        if(empty($errors)){
+            $result = Model::insert(self::$table, ["username"=> $username,"password"=> $password,"email"=> $email]);
+            return $result;
+        }else{
+            return false;
+        }
+
+
     }
 
     public static function update($id, $username, $password, $email){
-        $result = Model::update(
-            self::$table,
-             array_merge(
-                $username ? ["username"=> $username] : [],
-                $password ? ["password"=> $password] : [],
-                $email ? ["email"=> $email] : []
-            ),
-            ["user_id"=> $id]
-        );
-        return $result;
+        if($username && !self::isUsernameValid($username)){
+            $errors[] = "Username is not valid (minimum 4 characters), only . and _ allowed";
+        }else if ($username && !self::isUsernameUnique($username)){
+            $errors[] = "Username is note unique";
+        }
+        if($email && !self::isEmailUnique($email)){
+            $errors[] = "Email is allready used";
+        }
+        if($password && !self::isPasswordValid($password)){
+            $errors[] = "Password must contain at least  8 characters, 1 lowercase, 1 uppercase, 1 number and 1 special characters (@$!%*?&)";
+        }
+
+        if(empty($errors)){
+            $result = Model::update(
+                self::$table,
+                array_merge(
+                    $username ? ["username"=> $username] : [],
+                    $password ? ["password"=> $password] : [],
+                    $email ? ["email"=> $email] : []
+                ),
+                ["user_id"=> $id]
+            );
+            return $result;
+        }else{
+            return false;
+        }
     }
 
     public static function delete($id){
         $result = Model::delete(self::$table, ["user_id"=> $id]);
         return $result;
+    }
+
+    public static function isPasswordValid($password){
+        return preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $password);
+    }
+
+    public static function isUsernameValid($username){
+        return preg_match("/^[a-zA-Z0-9._]{4,}$/", $username);
+    }
+
+    public static function isEmailUnique($email){
+        return Model::find(self::$table, ["email"=> $email]) !== null;
+    }
+
+    public static function isUsernameUnique($username){
+        return Model::find(self::$table, ["username" => $username]) !== null;
     }
 }
