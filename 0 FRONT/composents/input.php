b1fbfc139989cf289e22787ba95cc55229fad56a
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-17 19:27:02 +0100

    :art: HTML de la page Register

--- 0 FRONT/composents/input.php
+++ 0 FRONT/composents/input.php
@@ -1,9 +1,9 @@
 <?php {
-    function input($name, $placeHolder, $type = "text", $default = "", $styles=""){
+    function input($name, $placeHolder, $type = "text", $default = "", $styles="", $required=false){
         ?>
         <div class="input flex flex-col p-8 items-start gap-4 bg-white radius-10 border-2 border-gray w-full">
             <label class="text-10 w-full text-black" for="<?=$name?>"><?=$name?></label>
-            <input class="text-14 text-gray w-full" type=<?=$type?> name="<?=$name?>" id="<?=$name?>" value="<?=$default?>" placeholder="<?=$placeHolder?>">
+            <input class="text-14 text-gray w-full" type="<?=$type?>" name="<?=$name?>" id="<?=$name?>" value="<?=$default?>" placeholder="<?=$placeHolder?>" <?= $required ? "required" : "" ?>>
         </div>
         <?php
     }
