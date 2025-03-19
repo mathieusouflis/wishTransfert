b9487358594f26a490295a858f21523da09129da
Zaco <cyrilmathe08@gmail.com>
2025-03-19 10:09:19 +0100

    :bug: ajout des icones

--- settings.php
+++ settings.php
@@ -9,11 +9,11 @@ require_once '0 FRONT/base/header.php';
 <div class="h-410 w-561 gap-10 page-left">
     <div class="w-171 h-410 bg-white border-radius20 flex flex-col justify-between">
         <div class="p-16">
-            <h3 class="flex items-center text-20 text-black button-bg-gray h-34">Profile</h3>
-            <h3 class="flex items-center text-20 text-black button-bg-gray h-34">Links created</h3>
+            <h3 class="w-full h-34 flex items-center text-15 text-black button-bg-gray h-34 gap-10"><?php icon("person") ?>Profile</h3>
+            <h3 class="w-full h-34 flex items-center text-15 text-black button-bg-gray h-34 gap-10"><?php icon("link") ?>Links created</h3>
         </div>
         <div class="flex items-center p-16 border-top">
-            <h3 class="text-20 text-red">Disconnect</h3>
+            <h3 class="w-full h-34 flex items-center text-15 text-red button-bg-gray gap-10"><?php icon("trash") ?>Disconnect</h3>
         </div>
     </div>
     <div class="page-left3">
