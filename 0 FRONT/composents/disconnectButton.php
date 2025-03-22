52547953925937ac613a17e2614afe484d3f18b1
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-23 00:27:53 +0100

    :bug: Fix redirection on disconnect...

--- 0 FRONT/composents/disconnectButton.php
+++ 0 FRONT/composents/disconnectButton.php
@@ -1,11 +1,11 @@
 <?php
 require_once "./0 FRONT/composents/buttons.php";
-if(session_status() === PHP_SESSION_NONE) session_start();
+require_once "./config/config.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     session_unset();
     session_destroy();
-    header('Location: login.php');
+    echo '<script>window.location.href = "'.APP_URL.'login.php";</script>';
     exit;
 }
 ?>
